466c7e581f664622e7b190dd9319aea4d8ed2c50
PRATIK RAJ <kpratik071997@gmail.com>
2025-07-16 09:48:32 +0530

    project created

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "../src/components/ui/Toaster.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
